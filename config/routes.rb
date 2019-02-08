52592d8f21db5115c34ec5c62495d51b739e38ce
mike <michaeldotson@protonmail.com>
2019-02-07 23:25:20 -0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :posts
+  # STEP 1: A ROUTE triggers a controller action
+  # verb "/urls" => "namespace/controllers#action"
+
+  # get "/posts" => "posts#index"
+end
